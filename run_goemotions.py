b3fd109657267e97ab73755b8c60b6c15e37418d
Andrei Chernov <andrei.chernov@flix.com>
2024-11-29 19:43:47 +0100

    added print loss

--- run_goemotions.py
+++ run_goemotions.py
@@ -108,6 +108,7 @@ def train(args,
                 loss = loss / args.gradient_accumulation_steps
 
             logger.info(f"loss; {loss}")
+            print(f"loss; {loss}")
             loss.backward()
             tr_loss += loss.item()
             if (step + 1) % args.gradient_accumulation_steps == 0 or (
